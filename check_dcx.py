51f20a574eeddbe2c7c91cee6c2530761c400043
Jan <janrubio@utexas.edu>
2023-06-20 14:32:00 -0500

    DCXer: make a dcx (first version)

--- check_dcx.py
+++ check_dcx.py
@@ -1,16 +1,15 @@
 from os.path import exists
 from _io import TextIOWrapper as File
+from math import floor
 
-CONTENT = "/home/vislab/Gallery/Content/"
-COMMON = "/home/vislab/Gallery/common_content/"
+# CONTENT = "/mnt/c/Users/jan/Desktop/Files/Work_Material/Employment/Visualizations_Lab/Display_Cluster/DCXer/Content/"
+# COMMON = "/mnt/c/Users/jan/Desktop/Files/Work_Material/Employment/Visualizations_Lab/Display_Cluster/DCXer/Common/"
 
 ''' Determine whether FILE_NAME exists and where. '''
-def is_stallion_file(file_name:str):
-    if(exists(CONTENT + file_name)):
-        return (True, CONTENT + file_name)
-    elif(exists(COMMON + file_name)):
-        return (True, COMMON + file_name)
-    return (False, "")
+def is_stallion_file(file_dir:str):
+    if(exists(file_dir)):
+        return True
+    return False
 
 ''' Create and initialize a dcx file of name DCX_NAME. '''
 def create_dcx(dcx_name:str) -> File:
@@ -19,18 +18,42 @@ def create_dcx(dcx_name:str) -> File:
     return file_ptr
 
 ''' Add file FILE_PATH to the dcx file FILE. '''
-def write_to_dcx(dcx_file:File, file_path:str):
+def write_to_dcx(dcx_file:File, file_path:str, x_coord, y_coord, width, height):
+
+    # append hearder opener of dcx file
     content_window = " <ContentWindow>\n"
-    content_window += " <URI>" + file_path + "</URI>\n"
-    
-    content_window += "    <centerX>0.5</centerX>\n    <centerY>0.5</centerY>\n  <zoom>1</zoom>\n  <selected>0</selected>\n <ContentWindow>\n"
+
+    # append associated location of file
+    content_window += "  <URI>" + file_path + "</URI>\n"
+
+    # append x & y location and width & height
+    content_window += "  <x>" + str(float(x_coord)) + "</x>\n"
+    content_window += "  <y>" + str(float(y_coord)) + "</y>\n"
+    content_window += "  <w>" + str(float(width)) + "</w>\n"
+    content_window += "  <h>" + str(float(height)) + "</h>\n"
+
+    # append center, zoom, and selection (whatever that means)
+    content_window += "  <centerX>0.5</centerX>\n  <centerY>0.5</centerY>\n  <zoom>1</zoom>\n  <selected>0</selected>\n"
+
+    # append header closer of dcx file
+    content_window += " </ContentWindow>\n"
     dcx_file.write(content_window)
 
+''' Close the dcx file of name DCX_NAME. '''
 def close_dcx(dcx_file:File):
     dcx_file.write("</state>")
     dcx_file.close()
 
-file_ptr = create_dcx("hello.txt")
-write_to_dcx(file_ptr, "hello, world!")
-write_to_dcx(file_ptr, "hi!")
-close_dcx(file_ptr)
+''' Calculate the position and dimensions '''
+def get_pos_and_dims(i:int, x_dim:int, y_dim:int):
+    if(i < x_dim * y_dim):
+        return float('%.3f'%(0.167 * (i%x_dim))), float('%.3f'%(0.337 * floor(i/x_dim))), 1.0, 0.326
+    return 0.0, float('%.3f'%(0.674+0.337)), 1.0, 1.0
+
+# file_ptr = create_dcx("hello.txt")
+# write_to_dcx(file_ptr, "hello, world!", 1.01, 2.02, 3.1, 4.2)
+# write_to_dcx(file_ptr, "hi!", 2.01, 1.02, 3.18, 0.2)
+# close_dcx(file_ptr)
+
+# for n in range(19):
+#     print(n, get_pos_and_dims(n, 6, 3))
--- make_dcx.py
+++ make_dcx.py
@@ -1,5 +1,14 @@
 import sys
 import numpy as np
+from check_dcx import is_stallion_file, create_dcx, write_to_dcx, close_dcx, get_pos_and_dims
+import tkinter as tk
+from tkinter import filedialog
+
+CONTENT_DIR = "/mnt/c/Users/jan/Desktop/Files/Work_Material/Employment/Visualizations_Lab/Display_Cluster/DCXer/Content/"
+DCX_DIR = "/mnt/c/Users/jan/Desktop/Files/Work_Material/Employment/Visualizations_Lab/Display_Cluster/DCXer/dcx_files/"
+
+STALLION_SCREEN_WIDTH = 6
+STALLION_SCREEN_HEIGHT = 3
 
 def main():
 
@@ -13,32 +22,60 @@ def main():
         dcx_names = sys.argv[1:len(sys.argv)]
         # print(file_names)
         
-        print()
         for dcx_name in dcx_names:
 
-            # Prompt user for DCX information
-            print("DCX file '" + dcx_name + "' will be created.")
-            file_amt = int(input("How many files will be in this file? "))
-            print("OK...\n")
-            
-            # files to add to dcx file
-            files = np.array([])
+            # Name of dcx
+            dcx_title = DCX_DIR + dcx_name
 
-            # prompt user the names of the files
-            for file_num in range(file_amt):
-                file_name = input("file " + str(file_num) + ": ")
-                files = np.append(files, file_name)
-            
-            print()
+            if(not is_stallion_file(dcx_title)):
 
-            # display array
-            for file_name in files:
-                print(file_name)
-                # make a dcx
-                # determine if file_name is an actual file
-                # add file into dcx
+                # Prompt user for DCX information
+                print("\nDCX file '" + dcx_name + "' will be created...")
+                # file_amt = int(input("How many files will be in this file? "))
+                print("Files selected are...\n")
 
-            # inform user of file creation
-            print("DCX file '" + dcx_name + "' has been created.\n")
+                # Make dcx
+                file_ptr = create_dcx(dcx_title)
+
+                # Prompt user to open files in the filesystem
+                tk.Tk().withdraw()
+                filepath = filedialog.askopenfilenames(title="Open a Text File", initialdir=CONTENT_DIR, filetypes=(("text files","*.txt"), ("all files","*.*")))
+                
+                # At least one file on the DCX
+                if(len(filepath) != 0):
+
+                    for fpi in range(len(filepath)):
+                        if(is_stallion_file(filepath[fpi])):
+
+                            # determine the coordinates based on the index
+                            x, y, w, h = get_pos_and_dims(fpi, STALLION_SCREEN_WIDTH, STALLION_SCREEN_HEIGHT)
+
+                            # print file location
+                            print(filepath[fpi])
+
+                            # add file to dcx
+                            write_to_dcx(file_ptr, filepath[fpi], x, y, w, h)
+
+                        else:
+                            print("file not found :(")
+                
+                # Close dcx
+                close_dcx(file_ptr)
+                print()
+
+                # display array
+                # for file_name in files:
+
+                    # determine if file_name is an actual file
+                    # is_stallion_file()
+
+                    # add file into dcx
+                    
+
+                # inform user of file creation
+                print("DCX file '" + dcx_name + "' has been created.\n")
+
+            else:
+                print("'" + dcx_title + "' already exists")
 
 main()
