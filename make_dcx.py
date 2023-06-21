aca4fc4d4e63324ad6c47ca53fc67016b579758f
andrewsolis <andrewjsolis13@gmail.com>
2023-06-21 16:51:45 -0500

    file dialog prompt uses 'Zenity' due to CentOS env

--- make_dcx.py
+++ make_dcx.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
+
 import sys
+import os
 import numpy as np
 from check_dcx import is_stallion_file, create_dcx, write_to_dcx, close_dcx, get_pos_and_dims
-import tkinter as tk
-from tkinter import filedialog
+import subprocess
 
-CONTENT_DIR = "/mnt/c/Users/jan/Desktop/Files/Work_Material/Employment/Visualizations_Lab/Display_Cluster/DCXer/Content/"
-DCX_DIR = "/mnt/c/Users/jan/Desktop/Files/Work_Material/Employment/Visualizations_Lab/Display_Cluster/DCXer/dcx_files/"
+CONTENT_DIR = "/home/vislab/Gallery/Content/"
+DCX_DIR = "/home/vislab/Gallery/jan-projects/DCXer/dcx_files/"
 
 STALLION_SCREEN_WIDTH = 6
 STALLION_SCREEN_HEIGHT = 3
@@ -38,42 +40,51 @@ def main():
                 file_ptr = create_dcx(dcx_title)
 
                 # Prompt user to open files in the filesystem
-                tk.Tk().withdraw()
-                filepath = filedialog.askopenfilenames(title="Open a Text File", initialdir=CONTENT_DIR, filetypes=(("text files","*.txt"), ("all files","*.*")))
-                
-                # At least one file on the DCX
-                if(len(filepath) != 0):
+                FILE_DIALOG_CMD = ["zenity", "--file-selection", "--multiple", "--text='Select Files...'"]
+                try:
+                    file_dialog_res = subprocess.check_output(FILE_DIALOG_CMD).decode("utf-8")
 
-                    for fpi in range(len(filepath)):
-                        if(is_stallion_file(filepath[fpi])):
+                    filepath = file_dialog_res[0:len(file_dialog_res)-1].split("|")
 
-                            # determine the coordinates based on the index
-                            x, y, w, h = get_pos_and_dims(fpi, STALLION_SCREEN_WIDTH, STALLION_SCREEN_HEIGHT)
+                    # tk.Tk().withdraw()
+                    # filepath = filedialog.askopenfilenames(title="Open a Text File", initialdir=CONTENT_DIR, filetypes=(("text files","*.txt"), ("all files","*.*")))
+			
+                    # At least one file on the DCX
+                    if(len(filepath) != 0):
 
-                            # print file location
-                            print(filepath[fpi])
+                        for fpi in range(len(filepath)):
+                            if(is_stallion_file(filepath[fpi])):
 
-                            # add file to dcx
-                            write_to_dcx(file_ptr, filepath[fpi], x, y, w, h)
+                                # determine the coordinates based on the index
+                                x, y, w, h = get_pos_and_dims(fpi, STALLION_SCREEN_WIDTH, STALLION_SCREEN_HEIGHT)
 
-                        else:
-                            print("file not found :(")
-                
-                # Close dcx
-                close_dcx(file_ptr)
-                print()
+                                # print file location
+                                print(filepath[fpi])
 
-                # display array
-                # for file_name in files:
+                                # add file to dcx
+                                write_to_dcx(file_ptr, filepath[fpi], x, y, w, h)
 
-                    # determine if file_name is an actual file
-                    # is_stallion_file()
+                            else:
+                                print("file not found :(")
 
-                    # add file into dcx
+                        # Close dcx
+                        close_dcx(file_ptr)
+
+                        # inform user of file creation
+                        print("\nDCX file '" + dcx_name + "' has been created.\n")
+
+                    # no files were selected
+                    else:
+                        close_dcx(file_ptr)
+                        #os.remove(dcx_title)
+                        print("\nDCX file was not created")
+
+                except subprocess.CalledProcessError as e:
+                    close_dcx(file_ptr)
+                    if(os.path.isfile(dcx_title)):
+                        os.remove(dcx_title)
+                    print("No files were selected. DCX file creation has been cancelled.")
                     
-
-                # inform user of file creation
-                print("DCX file '" + dcx_name + "' has been created.\n")
 
             else:
                 print("'" + dcx_title + "' already exists")
